71fe813e5da91e57648bdf2e190f3cac964ffe88
kumar-m-kiran <kumar.m.kiran@gmail.com>
2018-06-21 11:24:11 +0530

    Add description and result

--- examples/python/numpy_append_performance.py
+++ examples/python/numpy_append_performance.py
@@ -1,5 +1,29 @@
 #!/usr/bin/python
+'''
+Description : Script to
+1. show the performance bottleneck when numpy append is used directly
+2. efficient way to handle the loading of numpy array
 
+How to Run and sample result
+
+blr-lvtb9:~/workspace/git/techunravel/examples/python$ python numpy_append_performance.py
+-------------------------------
+Time taken to directly append to numpy [0.0650050640106], nr elements after append [10000]
+Time taken to indirectly append to numpy [0.00114607810974], nr elements after append [10000]
+-------------------------------
+Time taken to directly append to numpy [0.21977686882], nr elements after append [20000]
+Time taken to indirectly append to numpy [0.00235295295715], nr elements after append [20000]
+-------------------------------
+Time taken to directly append to numpy [0.663153886795], nr elements after append [30000]
+Time taken to indirectly append to numpy [0.00346493721008], nr elements after append [30000]
+-------------------------------
+Time taken to directly append to numpy [1.36484885216], nr elements after append [40000]
+Time taken to indirectly append to numpy [0.00476002693176], nr elements after append [40000]
+-------------------------------
+Time taken to directly append to numpy [2.40069913864], nr elements after append [50000]
+Time taken to indirectly append to numpy [0.00593090057373], nr elements after append [50000]
+
+'''
 import numpy as np
 import time as tm
 
